db2b46210c48c028cd87295449719f1cd086d064
kristenfisch <kristenfischbuch@gmail.com>
2017-04-06 02:57:05 +0000

    Adding params hash to signup

--- app/actions.rb
+++ app/actions.rb
@@ -10,3 +10,15 @@ get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
 end
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end 
+
+post '/signup' do
+    params.to_s
+end
+
+
+
